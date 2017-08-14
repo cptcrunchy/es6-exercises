6096714f7f8572af3052e86704bda51f4c6bdbc4
Jason Gutierrez <jasong84@gmail.com>
2017-08-14 01:14:11 -0500

    docs (continued exercise on object destructuring)

--- app/js/index.js
+++ app/js/index.js
@@ -3,17 +3,37 @@ require('styles/main.scss');
 /* js */
 import { log, logTitle } from 'logger';
 /* your imports */
-logTitle('Array Destructuring');
+logTitle('Object Destructuring');
 /* coding examples */
+const getUser = () => {
+  return {
+    name: 'John',
+    surname: 'Doe',
+    gender: 'male',
+    address: {
+      country: 'United States',
+      city: 'California',
+      postCode: 'CA',
+      fullAddress: {
+        doorNumber: 22,
+        street: 'LA st'
+      }
+    },
+    age: 29
+  }
+};
 
-const names = ['Anna', 'Mariam', 'Joe', 'Mark', 'Matt'];
+const user = getUser();
 
-// const anna = names[0];
-// const mariam = names[1];
-// const joe = names[2];
+// const name = user.name;
+// const age = user.age;
+// const country = user.address.country;
+const doorNumber = user.address.fullAddress.doorNumber;
 
-const [a,m,j, ...restOfNames] = names;
+const { name, age, address: {country: theCountry } } = user;
+const { address: { fullAddress: { doorNumber: number}}} = user;
 
-log(`${a} ${m} ${j}`);
-log(restOfNames);
-
+log(name);
+log(age);
+log(theCountry);
+log(number);
