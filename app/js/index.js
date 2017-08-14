891b10c7343a38e8144b2659ccbdf22be933370b
Jason Gutierrez <jasong84@gmail.com>
2017-08-13 21:04:51 -0500

    docs (example of template literals )

--- app/js/index.js
+++ app/js/index.js
@@ -4,5 +4,11 @@ require('styles/main.scss');
 import $ from 'jquery';
 import { log, logTitle } from 'logger';
 /* your imports */
-logTitle('Title');
+logTitle('ECMAScript 2015');
 /* coding examples */
+const name = 'Jason';
+const city = 'San Antonio';
+const age = 33;
+
+log("Name: " + name + " City: " + city + " Age: " + age);
+log(`Name: ${name} City: ${city} Age: ${age}`);
