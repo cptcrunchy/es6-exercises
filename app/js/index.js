c810ac20c08a12bab3a68dbfdc8b42434bc4edf0
Jason Gutierrez <jasong84@gmail.com>
2017-08-14 01:19:06 -0500

    docs (simple exercise on Function Default Parameters)

--- app/js/index.js
+++ app/js/index.js
@@ -3,37 +3,13 @@ require('styles/main.scss');
 /* js */
 import { log, logTitle } from 'logger';
 /* your imports */
-logTitle('Object Destructuring');
+logTitle('Function Default Parameters');
 /* coding examples */
-const getUser = () => {
-  return {
-    name: 'John',
-    surname: 'Doe',
-    gender: 'male',
-    address: {
-      country: 'United States',
-      city: 'California',
-      postCode: 'CA',
-      fullAddress: {
-        doorNumber: 22,
-        street: 'LA st'
-      }
-    },
-    age: 29
-  }
-};
+const calculatePay = (yearSalary, bonus = {
+  teamBonus: 0,
+  employeeBonus: 0
+}) => {
+  return yearSalary + bonus.teamBonus + bonus.employeeBonus;
+}
 
-const user = getUser();
-
-// const name = user.name;
-// const age = user.age;
-// const country = user.address.country;
-const doorNumber = user.address.fullAddress.doorNumber;
-
-const { name, age, address: {country: theCountry } } = user;
-const { address: { fullAddress: { doorNumber: number}}} = user;
-
-log(name);
-log(age);
-log(theCountry);
-log(number);
+log(calculatePay(22000, {teamBonus: 10000, employeeBonus: 6000}));
