badaebed53f8f7e707c460a35f0ae5e8787b0f4b
Jason Gutierrez <jasong84@gmail.com>
2017-08-13 21:12:36 -0500

    docs ( exercise on using the spread operator on arrays)

--- app/js/index.js
+++ app/js/index.js
@@ -6,9 +6,28 @@ import { log, logTitle } from 'logger';
 /* your imports */
 logTitle('ECMAScript 2015');
 /* coding examples */
-const name = 'Jason';
-const city = 'San Antonio';
-const age = 33;
+const arrayOne = ['James', 'Jenny', 'Jessica', 'Jerry'];
+const arrayTwo = ['Oscar', 'Marissa', 'Chey', 'Dorothy'];
 
-log("Name: " + name + " City: " + city + " Age: " + age);
-log(`Name: ${name} City: ${city} Age: ${age}`);
+const concatArray = [...arrayOne, ...arrayTwo];
+
+concatArray.forEach(function(name) {
+  log(name);
+});
+
+const name = 'Mario';
+const nameToArray = [...name];
+nameToArray.forEach(function(letter){
+  log(letter);
+});
+
+const addNumbers = function(n1,n2,n3) {
+  return n1+n2+n3;
+}
+
+const numbers = [1,5,19];
+
+// const addition = addNumbers(numbers[0], numbers[1], number[2]);
+const addition = addNumbers(...numbers);
+
+log(addition);
